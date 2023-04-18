4b2b02a0821a6ddfef70014338b41af80c7a927a
coderkhayrul <coderkhayrul@gmail.com>
2023-04-19 02:18:48 +0600

    Nuxt Js Testing

---
+++ components/Button.vue
@@ -0,0 +1,13 @@
+<template>
+<button class="bg-blue-600">Button</button>
+</template>
+
+<script>
+export default {
+    name: "Button"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <h2>Home Pages</h2>
+  <Button />
+</template>
+
+<script>
+import Button from "~/components/Button.vue";
+
+export default {
+    name: "index",
+    components: {Button}
+}
+</script>
+
+<style scoped>
+
+</style>
